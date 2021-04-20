a27d779b2da01f6f4b0c33071d97b12e98244870
ladyymendez <ladyy.mendez@gmail.com>
2021-04-20 16:02:51 -0500

    Update Icons

--- src/components/MinuteControl/index.js
+++ src/components/MinuteControl/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Minus, Plus } from "../Icons";
 
 export default function MinuteControl({
   name,
@@ -9,13 +10,13 @@ export default function MinuteControl({
   return (
     <div className="flex flex-col items-center mb-4 lg:my-4">
       <div>{name}</div>
-      <div className="px-4">
+      <div className="px-4 flex items-center justify-center">
         <button
           disabled={seconds <= 60}
           className="bg-primary px-4 py-2 rounded-full focus:outline-none active:bg-pink-200"
           onClick={handleDecrement}
         >
-          -
+          <Minus />
         </button>
         <input
           className="w-10 text-black m-2 rounded px-2 outline-none"
@@ -28,7 +29,7 @@ export default function MinuteControl({
           className="bg-primary px-4 py-2 rounded-full focus:outline-none active:bg-pink-200"
           onClick={handleIncrement}
         >
-          +
+          <Plus />
         </button>
       </div>
     </div>
